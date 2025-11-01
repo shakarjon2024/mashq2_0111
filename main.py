ecc4682757a4107c3aced8ed6ed3f49d94499349
shakarjon2024 <rozibayevashakarjon1707@gmail.com>
2025-11-01 15:05:47 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# 1 - m
+numbers = (1, 2, 9, 3, 4, 7)
+elementlar_soni = len(numbers)
+print(elementlar_soni)
+
+
+# 2 - m
+numbers = (1, 6, 3, 7, 3)
+takrorlanish_soni = (numbers.count(3))
+print(takrorlanish_soni)
+
+
+# 3 - m
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+kopaytirish = tuple(x * 2 for x in numbers)
+print(kopaytirish)
